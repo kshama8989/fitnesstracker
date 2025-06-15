0e8eb4788e92cf59836933813c70bc272de1fb7d
Kshama <kshama.singh.jat@ibm.com>
2025-06-15 12:39:28 +0530

    UserSetup code changes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "../Pages/RegisterPage";
+import HomePage from "../Pages/HomePage";
 
 const AppRoutes =() =>{
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<RegisterPage />}/>
-                {/* <Route element={<PrivateRoute />}>
-                    <Route path="home" element={<BeforeLoginHome />} /> 
-                </Route> */}
+                {/* <Route element={<PrivateRoute />}> */}
+                    <Route path="home" element={<HomePage />} /> 
+                {/* </Route> */}
             </Routes>
         </Router>
     )
